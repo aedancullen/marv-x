3cc7938a5a78527e8270a7dc66008e5344a6193f
Aedan Cullen <aedancullen@gmail.com>
2018-12-08 03:27:29 -0500

    Add measured constants

--- src/main/java/org/firstinspires/ftc/teamcode/MarvNavConstants.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MarvNavConstants.java
@@ -4,20 +4,20 @@ public class MarvNavConstants {
 
     public static int QUADPACER_TPU = 110;
 
-    public static int EXPANDO_HORIZ_LIMIT = 910;
-    public static int EXPANDO_HORIZ_DUMP = 10;
+    public static int EXPANDO_HORIZ_LIMIT = 1900;
+    public static int EXPANDO_HORIZ_DUMP = 505;
 
-    public static int HORIZ_SPIN_CLEAR_MODULUS = 200;
-    public static int HORIZ_SPIN_READY_MODULUS = 100;
+    public static int HORIZ_SPIN_CLEAR_MODULUS = 192/4;
+    public static int HORIZ_SPIN_READY_MODULUS = 192/2;
     public static int HORIZ_SPIN_MODULUS_TOLERANCE = 10;
 
-    public static double HORIZ_BOX_TOUP_MILLIS = 200;
-    public static int HORIZ_BOX_TODOWN_MILLIS = 200;
+    public static double HORIZ_BOX_TOUP_MILLIS = 500;
+    public static int HORIZ_BOX_TODOWN_MILLIS = 500;
 
     public static double HORIZ_BOX_UP_NEUTRAL = 0.5;
     public static double HORIZ_BOX_UP_DUMPING = 1.0;
     public static double HORIZ_BOX_DOWN = 0.07;
-    public static double HORIZ_BOX_SAFE = 0;
+    public static double HORIZ_BOX_SAFE = 0.29;
 
     public static double VERT_BOX_UP_NEAR = 0;
     public static double VERT_BOX_UP_BOUND = 0;
@@ -37,14 +37,14 @@ public class MarvNavConstants {
     public static double EXPANDO_VERT_TODOWN_SPEED = 0.2;
     public static double EXPANDO_VERT_TOSAFE_SPEED = 1.0;
 
-    public static int VERT_SPIN_TODROP_MILLIS = 200;
+    public static int VERT_SPIN_TODROP_MILLIS = 500;
 
-    public static double VERT_SPIN_NEUTRAL = 0;
-    public static double VERT_SPIN_R2BACK = 0;
-    public static double VERT_SPIN_L2BACK = 0;
-    public static double VERT_SPIN_R2BACK_FARTHER = 0;
-    public static double VERT_SPIN_R2BACK_LESSER = 0;
+    public static double VERT_SPIN_NEUTRAL = 0.492;
+    public static double VERT_SPIN_R2BACK = 0.84;
+    public static double VERT_SPIN_L2BACK = 0.14;
+    public static double VERT_SPIN_R2BACK_FARTHER = 1;
+    public static double VERT_SPIN_R2BACK_LESSER = 0.68;
     public static double VERT_SPIN_L2BACK_FARTHER = 0;
-    public static double VERT_SPIN_L2BACK_LESSER = 0;
+    public static double VERT_SPIN_L2BACK_LESSER = 0.16;
 
 }
