8170b065cb191f43e353898e1c78d0b15db90123
Aedan Cullen <aedancullen@gmail.com>
2019-04-08 22:07:07 -0400

    Adjust constants

--- src/main/java/org/firstinspires/ftc/teamcode/MarvConstantsV3.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MarvConstantsV3.java
@@ -19,17 +19,17 @@ public class MarvConstantsV3 {
 
     public static double EXPANDO_DIAG_UP_POWER = 0.75;
     public static double EXPANDO_DIAG_SAFE_POWER = 0.75;
-    public static double EXPANDO_DIAG_STALIN_POWER = 0.15;
+    public static double EXPANDO_DIAG_STALIN_POWER = 0.22;
 
     public static long EXPANDO_DIAG_STALIN_TIME = 500;
-    public static long EXPANDO_DIAG_DROP_TIME = 500;
+    public static long EXPANDO_DIAG_DROP_TIME = 1000;
     public static long EXPANDO_DIAG_UNDROP_TIME = 500;
 
     public static long EXPANDO_DIAG_PAUSE_TIME = 500;
 
 
     public static double DROP_ANGLE_FLAT = 0.5;
-    public static double DROP_ANGLE_NEAR = 0.8;
+    public static double DROP_ANGLE_NEAR = 0.75;
     public static double DROP_ANGLE_FAR = 0.8;
     public static double DROP_ANGLE_MID = 0.8;
 
@@ -37,12 +37,12 @@ public class MarvConstantsV3 {
     public static double SWOP_ANGLE_SWOPPED;
 
     public static int EXPANDO_HORIZ_UP = 1800;
-    public static int EXPANDO_HORIZ_SAFE = 375;
+    public static int EXPANDO_HORIZ_SAFE = 390;
     public static int EXPANDO_HORIZ_FLYING_LIMIT = EXPANDO_HORIZ_UP / 2;
 
     public static double HORIZ_LIFT_UP_NEUTRAL = .42;
-    public static double HORIZ_LIFT_UP_WAITING = .91;
-    public static double HORIZ_LIFT_UP_DUMPING = .91;
+    public static double HORIZ_LIFT_UP_WAITING = .90;
+    public static double HORIZ_LIFT_UP_DUMPING = .90;
     public static double HORIZ_LIFT_DOWN = 0.16;
 
 
@@ -55,7 +55,7 @@ public class MarvConstantsV3 {
     public static double UC_HORIZSPIN_EJECT = 0.75;
     public static double UC_HORIZSPIN_INTAKE = 0.75;
     public static double UC_HORIZSPIN_TRANSFER = 0.75;
-    public static double UC_HORIZSPIN_HOLD = 0.12;
+    public static double UC_HORIZSPIN_HOLD = 0.0;
 
     public static int UC_EXPANDOHORIZ_BUF = 50;
 
