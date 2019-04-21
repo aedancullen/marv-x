7048928a2fce0c3b8339505d8eb40415e5978af9
Aedan Cullen <aedancullen@gmail.com>
2019-04-21 00:36:17 -0400

    Compensate for farther mineral

--- src/main/java/org/firstinspires/ftc/teamcode/MarvXAutoNearV3.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MarvXAutoNearV3.java
@@ -98,21 +98,24 @@ public class MarvXAutoNearV3 extends LinearOpMode {
         quadPacer.setRobotAttitude(ROBOT_INIT_ATTITUDE);
 
         apGoTo(new double[] {-2.5, 0, 0}, -Math.PI / 2, false, true, false);
-        //apGoTo(new double[] {-2, 2, 0}, -Math.PI / 2, true, true, false);
         apGoTo(new double[] {-36/3, 15, 0}, -Math.PI / 2, true, true, false);
         apGoTo(new double[] {-36-12, 15, 0}, -Math.PI / 2, true, true, true);
         halt();
-        apGoTo(new double[] {-36-12, 15, 0}, 3*Math.PI / 4, true, false, true);
+        apGoToWithIdle(new double[] {-36-12, 15, 0}, 3*Math.PI / 4, true, false, true);
         halt(); marker();
         if (doubleSample) {
             apGoTo(new double[] {-36-12, 15, 0}, Math.PI, true, false, true);
-            halt(); sampleRClose();
-            apGoToWithIdle(new double[] {-36, 15+12, 0}, -Math.PI / 2 + 0.05, true, true, true);
+            halt(); 
+            if (sampleRCloseIsFar()) {apGoTo(new double[] {-36-12, 15-7, 0}, Math.PI, false, true, false);halt();}
+            sampleRClose();
+            apGoToWithIdle(new double[] {-36, 15+12, 0}, -Math.PI / 2 + 0.07, true, true, true);
         }
         else {
-            apGoTo(new double[] {-36, 15+12, 0}, -Math.PI / 2 + 0.05, true, true, true);
+            apGoTo(new double[] {-36, 15+12, 0}, -Math.PI / 2 + 0.07, true, true, true);
         }
-        halt(); sampleLClose();
+        halt(); 
+        if (sampleLCloseIsFar()) {apGoTo(new double[] {-36+7, 15+12, 0}, -Math.PI / 2 + 0.07, false, true, false);halt();}
+        sampleLClose();
 
         while(opModeIsActive()){idleIntakeAutomation();}
 
@@ -145,6 +148,13 @@ public class MarvXAutoNearV3 extends LinearOpMode {
             marv.runIntakeAutomation(0.66, false, false, false, false, false);
             marv.runAutomation(false, false);
         }
+        if (sampleTarget >= MarvConstantsV3.EXPANDO_HORIZ_FLYING_LIMIT) {
+            while (marv.expandoHorizL.getCurrentPosition() > sampleTarget) {
+                marv.runIntakeAutomation(-0.66, false, false, false, false, false);
+            }
+            marv.runIntakeAutomation(0, true, false, false, false, false);
+            sleep((int)((double)MarvConstantsV3.EHSM_UP/2.0));
+        }
         while (marv.intakeState != MarvXCommonV3.IntakeState.TRANSFER) {
             marv.runIntakeAutomation(0.0, false, false, false, false, true);
             marv.runAutomation(false, false);
@@ -167,6 +177,10 @@ public class MarvXAutoNearV3 extends LinearOpMode {
         }
     }
 
+    public boolean sampleLCloseIsFar() {
+        return res == 2;
+    }
+
     public void sampleRClose() {
         if (res == 2) {
             sample(MarvConstantsV3.AUTO_SAMPLE_NEAR, true);
@@ -177,6 +191,10 @@ public class MarvXAutoNearV3 extends LinearOpMode {
         else if (res == 0) {
             sample(MarvConstantsV3.AUTO_SAMPLE_FAR, true);
         }
+    }
+
+    public boolean sampleRCloseIsFar() {
+        return res == 0;
     }
 
     public void apGoTo(double[] pos, double hdg, boolean useOrientation, boolean useTranslation, boolean fullStop) {
