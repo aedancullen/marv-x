9cb56859b4abbaaa41936c84b3a5e497c73a0a18
Aedan Cullen <aedancullen@gmail.com>
2019-04-21 20:54:59 -0400

    Add far-auto file

--- src/main/java/org/firstinspires/ftc/teamcode/MarvXAutoCommonV3.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MarvXAutoCommonV3.java
@@ -29,6 +29,80 @@ public class MarvXAutoCommonV3 extends LinearOpMode {
 
     public void runOpMode() {}
 
+    public void runDepot() {
+        marv = new MarvXCommonV3(hardwareMap, true);
+        mineralFind = new MineralFind(hardwareMap);
+
+        marv.imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.RADIANS;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+
+        marv.imu.initialize(parameters);
+
+        autopilot = new AutopilotHost(telemetry);
+        quadPacer = new AutopilotTrackerQP37i(marv.getQuadPacerMotorX(), marv.getQuadPacerMotorY(), MarvConstantsV3.QUADPACER_POS, MarvConstantsV3.QUADPACER_TPU, marv.imu, MarvConstantsV3.QUADPACER_SUBSTEPS);
+        ((AutopilotTrackerQP37i)quadPacer).setInverts(false, false);
+        autopilot.setCountsToStable(MarvConstantsV3.AP_COUNTS_TO_STABLE);
+        autopilot.setNavigationUnitsToStable(MarvConstantsV3.AP_NAV_UNITS_TO_STABLE);
+        autopilot.setOrientationUnitsToStable(MarvConstantsV3.AP_ORIENT_UNITS_TO_STABLE);
+
+        mineralFind.detectInitInternal();
+        mineralFind.tfod.activate();
+
+        while(!opModeIsActive()) {
+            int detect = mineralFind.detectLoopInternal();
+            if (detect != -1) {
+                res = detect;
+            }
+            telemetry.addData("Mineral", res);
+
+            telemetry.addData("Press BACK to OPEN","");
+            telemetry.addData("Press a JOYSTICK to LOCK","");
+            telemetry.update();
+            if (isStopRequested()) {
+                mineralFind.detectStopInternal();
+                return;
+            }
+
+            if (gamepad2.back) {
+                hookState = HookState.OPEN;
+            }
+            else if (gamepad2.right_stick_button || gamepad2.left_stick_button) {
+                hookState = HookState.LOCK;
+            }
+
+            if (hookState == HookState.OPEN) {
+                if (marv.expandoVert.getCurrentPosition() > -2000) {
+                     marv.expandoVert.setPower(-1);
+                }
+                else {marv.expandoVert.setPower(0);}
+            }
+            else if (hookState == HookState.LOCK) {
+                if (marv.expandoVertCanLift()) {
+                    marv.expandoVert.setPower(1);
+                }
+                else {marv.expandoVert.setPower(0);}
+            }
+        }
+
+        mineralFind.detectStopInternal();
+
+        marv.expandoVert.setPower(-1);
+        while (marv.dist.getVoltage() < 2.25 && opModeIsActive() && marv.expandoVertCanDrop()) {idle();}
+        while (marv.dist.getVoltage() > 2.2 && opModeIsActive() && marv.expandoVertCanDrop()) {idle();}
+        int spos = marv.expandoVert.getCurrentPosition();
+        while (marv.expandoVert.getCurrentPosition() > spos - MarvConstantsV3.EXPANDO_VERT_EXTRA && marv.expandoVertCanDrop()) {idle();}
+        marv.expandoVert.setPower(0);
+        autopilot.communicate(quadPacer);
+        quadPacer.setRobotPosition(ROBOT_INIT_POSITION);
+        quadPacer.setRobotAttitude(ROBOT_INIT_ATTITUDE);
+
+        apGoTo(new double[] {-2.5, 0, 0}, -Math.PI / 2, false, true, false);
+        apGoTo(new double[] {-36/3, 15, 0}, -Math.PI / 2, true, true, false);
+
+    }
+
     public void runCrater() {
         marv = new MarvXCommonV3(hardwareMap, true);
         mineralFind = new MineralFind(hardwareMap);
---
+++ src/main/java/org/firstinspires/ftc/teamcode/MarvXAutoFarV3.java
@@ -0,0 +1,10 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.MarvXAutoCommonV3;
+
+@Autonomous(name="Far")
+public class MarvXAutoFarV3 extends MarvXAutoCommonV3 {
+    public void runOpMode() {
+        runDepot();
+    }
+}        
